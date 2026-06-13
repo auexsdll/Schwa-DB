fe1ced2eea28fd3b3edff365af398b97c7bb7b0e
au3xsdll <eminhatip8@gmail.com>
2026-06-13 15:38:42 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Middleware
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // CORS Configuration
 app.use(cors());
